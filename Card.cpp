c3b89da1458c180c49e7b3c51943c5170e7fa073
214534304 <ntuthuko.msimango@gmail.com>
2016-03-14 10:21:15 +0200

    my card class created

---
+++ Card.cpp
@@ -0,0 +1,22 @@
+#include "Card.h"
+
+
+string colour(){
+
+}
+int number(){
+
+}
+void print(){
+
+}
+
+
+Card::Card()
+{
+}
+
+
+Card::~Card()
+{
+}
---
+++ Card.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Card
+{
+
+public:
+	string colour();
+	int number();
+	void print();
+	Card();
+	~Card();
+};
+
